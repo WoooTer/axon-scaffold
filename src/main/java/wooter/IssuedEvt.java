8df096d1daf1eea9fe45ba51efe7340d06470732
WoooTer <wt7467571270>
2018-10-30 21:41:06 +0800

    first commit

---
+++ src/main/java/wooter/IssuedEvt.java
@@ -0,0 +1,20 @@
+package wooter;
+
+public class IssuedEvt {
+
+    private final String id;
+    private final Integer amount;
+
+    public IssuedEvt(String id, Integer amount) {
+        this.id = id;
+        this.amount = amount;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+}
